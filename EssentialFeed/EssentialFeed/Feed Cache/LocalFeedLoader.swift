57b48dc10545be84d43dd1ecd454208cd48bc6c5
MirceaX2Mobile <mircea.dragota@x2mobile.net>
2021-12-30 19:04:56 +0200

    finished performing Caledarical Calculations lecture

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -7,8 +7,9 @@ import Foundation
 public final class LocalFeedLoader {
 	private let store: FeedStore
 	private let currentDate: () -> Date
-	
+    private let calendar = Calendar(identifier: .gregorian)
 	public typealias SaveResult = Error?
+    public typealias LoadResult = LoadFeedResult
 	
 	public init(store: FeedStore, currentDate: @escaping () -> Date) {
 		self.store = store
@@ -26,14 +27,40 @@ public final class LocalFeedLoader {
 			}
 		}
 	}
-	
-	private func cache(_ feed: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
-		store.insert(feed.toLocal(), timestamp: currentDate()) { [weak self] error in
-			guard self != nil else { return }
-			
-			completion(error)
-		}
-	}
+    
+    private func cache(_ feed: [FeedImage], with completion: @escaping (SaveResult) -> Void) {
+        store.insert(feed.toLocal(), timestamp: currentDate()) { [weak self] error in
+            guard self != nil else { return }
+            
+            completion(error)
+        }
+    }
+    
+    public func load(completion: @escaping (LoadResult) -> Void) {
+        store.retrieve { [unowned self] result in
+            switch result {
+            case let .failure(error):
+                completion(.failure(error))
+                
+            case let .found(feed, timestamp) where self.validate(timestamp):
+                completion(.success(feed.toModels()))
+                
+            case .found, .empty:
+                completion(.success([]))
+            }
+        }
+    }
+    
+    private var maxCacheAgeInDays: Int {
+        return 7
+    }
+    
+    private func validate(_ timestamp: Date) -> Bool {
+        guard let maxCacheAge = calendar.date(byAdding: .day, value: 7, to: timestamp) else {
+            return false
+        }
+        return currentDate() < maxCacheAge
+    }
 }
 
 private extension Array where Element == FeedImage {
@@ -41,3 +68,10 @@ private extension Array where Element == FeedImage {
 		return map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
 	}
 }
+
+private extension Array where Element == LocalFeedImage {
+    func toModels() -> [FeedImage] {
+        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    }
+}
+
