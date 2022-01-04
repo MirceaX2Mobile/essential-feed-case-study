db2da1bab0cbd07892cde7ebe174c516435eab21
MirceaX2Mobile <mircea.dragota@x2mobile.net>
2022-01-04 09:30:56 +0200

    finished Separating Queries lecture

---
+++ EssentialFeed/EssentialFeedTests/Helpers/SharedTestHelpers.swift
@@ -0,0 +1,17 @@
+//
+//  SharedTestHelpers.swift
+//  EssentialFeedTests
+//
+//  Created by Mircea Dragota on 04.01.2022.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import Foundation
+
+func anyNSError() -> NSError {
+    return NSError(domain: "any error", code: 0)
+}
+ 
+func anyURL() -> URL {
+    return URL(string: "http://any-url.com")!
+}
