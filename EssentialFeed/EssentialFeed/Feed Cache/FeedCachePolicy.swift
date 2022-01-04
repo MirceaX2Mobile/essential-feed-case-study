a4f5fb24d7969aad8cb3e96b8ddfceec9672c496
MirceaX2Mobile <mircea.dragota@x2mobile.net>
2022-01-04 12:12:09 +0200

    finished Separating App-specific, App-agnostic lecture

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedCachePolicy.swift
@@ -0,0 +1,9 @@
+//
+//  FeedCachePolicy.swift
+//  EssentialFeed
+//
+//  Created by Mircea Dragota on 04.01.2022.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import Foundation
