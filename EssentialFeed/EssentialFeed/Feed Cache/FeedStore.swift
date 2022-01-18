190f3a5540cff631ae05c9545589112d32dac430
MirceaX2Mobile <mircea.dragota@x2mobile.net>
2022-01-18 13:00:47 +0200

    modified feed store retrieve result

--- EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -4,24 +4,25 @@
 
 import Foundation
 
-public enum RetrieveCachedFeedResult {
-    case empty
-    case found(feed: [LocalFeedImage], timestamp: Date)
-    case failure(Error)
-}
+public typealias CachedFeed = (feed: [LocalFeedImage], timestamp: Date)
 
 public protocol FeedStore {
-	typealias DeletionCompletion = (Error?) -> Void
-	typealias InsertionCompletion = (Error?) -> Void
-    typealias RetrievalCompletion = (RetrieveCachedFeedResult) -> Void
-	
-    /// The completion handler can be invoked in any thread.
-    /// Clients are responsible to sidpatch to appropriate threads, if needed.
-	func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    typealias DeletionResult = Error?
+    typealias DeletionCompletion = (DeletionResult) -> Void
+    
+    typealias InsertionResult = Error?
+    typealias InsertionCompletion = (InsertionResult) -> Void
+    
+    typealias RetrieveResult = Result<CachedFeed?, Error>
+    typealias RetrievalCompletion = (RetrieveResult) -> Void
     
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to sidpatch to appropriate threads, if needed.
-	func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    
+    /// The completion handler can be invoked in any thread.
+    /// Clients are responsible to sidpatch to appropriate threads, if needed.
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion)
     
     /// The completion handler can be invoked in any thread.
     /// Clients are responsible to sidpatch to appropriate threads, if needed.
