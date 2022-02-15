a2a008839848d006b6c9891626db707f2600c018
Mircea Dragota <mircea.dragota@x2mobile.net>
2022-02-15 14:09:05 +0200

    finished lecture Effectively Test-driving MVC UI

---
+++ EssentialFeed/EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,30 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Mircea Dragota on 28.01.2022.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+    public let feedImageContainer = UIView()
+    public let feedImageView = UIImageView()
+    
+    private(set) public lazy var feedImageRetryButton: UIButton = {
+        let button = UIButton()
+        button.addTarget(self, action: #selector(retryButtonTapped), for: .touchUpInside)
+        return button
+    }()
+    
+    var onRetry: (() -> Void)?
+    
+    @objc private func retryButtonTapped() {
+        onRetry?()
+    }
+}
