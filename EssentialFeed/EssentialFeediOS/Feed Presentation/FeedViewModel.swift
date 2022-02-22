b5cf6f4db76d8cbe8be89494e2894286c33feac9
Mircea Dragota <mircea.dragota@x2mobile.net>
2022-02-22 18:51:13 +0200

    added FeedImagePresenter

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Mircea Dragota on 22.02.2022.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
