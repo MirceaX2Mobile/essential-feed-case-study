7af5584f803999345cefe6a828dddc6e46e39aec
Mircea Dragota <mircea.dragota@x2mobile.net>
2022-02-24 16:33:53 +0200

    started adding UI using storyboard

--- EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
+++ EssentialFeed/EssentialFeediOS/Feed UI/Controllers/FeedViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 final public class FeedViewController: UITableViewController, UITableViewDataSourcePrefetching {
-    private var refreshController: FeedRefreshViewController?
+    @IBOutlet var refreshController: FeedRefreshViewController?
     var tableModel = [FeedImageCellController]() {
         didSet {
             tableView.reloadData()
@@ -17,15 +17,9 @@ final public class FeedViewController: UITableViewController, UITableViewDataSou
     }
     private var cellControllers = [IndexPath: FeedImageCellController]()
     
-    convenience init(refreshController: FeedRefreshViewController) {
-        self.init()
-        self.refreshController = refreshController
-    }
-    
     public override func viewDidLoad() {
         super.viewDidLoad()
-        
-        refreshControl = refreshController?.view
+
         tableView.prefetchDataSource = self
         refreshController?.refresh()
     }
