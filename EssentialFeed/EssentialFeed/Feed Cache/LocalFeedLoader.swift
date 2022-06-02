8785c8287366756ce5d86f62342dfeacf9290844
Mircea Dragota <mircea.dragota@x2mobile.net>
2022-06-02 17:22:48 +0300

    added Local Feed Image Data Loader with a Core Data infrastructure implementation

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -5,13 +5,13 @@
 import Foundation
 
 public final class LocalFeedLoader {
-	private let store: FeedStore
+    private let store: FeedStore
     private let currentDate: () -> Date
-	
-	public init(store: FeedStore, currentDate: @escaping () -> Date) {
-		self.store = store
+    
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+        self.store = store
         self.currentDate = currentDate
-	}
+    }
 }
 
 extension LocalFeedLoader: FeedLoader {
@@ -61,27 +61,30 @@ extension LocalFeedLoader {
 }
 
 extension LocalFeedLoader {
-    public func validateCache() {
+    public typealias ValidationResult = Result<Void, Error>
+    
+    public func validateCache(completion: @escaping (ValidationResult) -> Void) {
         store.retrieve { [weak self] result in
             guard let self = self else { return }
             
             switch result {
             case .failure:
-                self.store.deleteCachedFeed { _ in }
-            
-            case let .success(.some(cache)) where !FeedCachePolicy.validate(cache.timestamp, against: self.currentDate()):
-                self.store.deleteCachedFeed { _ in }
+                self.store.deleteCachedFeed(completion: completion)
                 
-            case .success: break
+            case let .success(.some(cache)) where !FeedCachePolicy.validate(cache.timestamp, against: self.currentDate()):
+                self.store.deleteCachedFeed(completion: completion)
+                
+            case .success:
+                completion(.success(()))
             }
         }
     }
 }
 
 private extension Array where Element == FeedImage {
-	func toLocal() -> [LocalFeedImage] {
-		return map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
-	}
+    func toLocal() -> [LocalFeedImage] {
+        return map { LocalFeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.url) }
+    }
 }
 
 private extension Array where Element == LocalFeedImage {
