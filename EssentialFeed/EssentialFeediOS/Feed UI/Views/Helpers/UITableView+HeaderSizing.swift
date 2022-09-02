8bec1cc2afef02377c868a52ff25ee40c880d07a
Mircea Dragota <mircea.dragota@x2mobile.net>
2022-09-02 14:29:57 +0300

    assert recorded snapshots

---
+++ EssentialFeed/EssentialFeediOS/Feed UI/Views/Helpers/UITableView+HeaderSizing.swift
@@ -0,0 +1,9 @@
+//
+//  UITableView+HeaderSizing.swift
+//  EssentialFeediOS
+//
+//  Created by Mircea Dragota on 02.09.2022.
+//  Copyright © 2022 Essential Developer. All rights reserved.
+//
+
+import Foundation
