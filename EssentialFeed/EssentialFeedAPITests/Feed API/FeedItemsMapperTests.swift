2b1d3cdef1fe8dd0272d571a57911c78d57c32bd
Mircea Dragota <mircea.dragota@x2mobile.net>
2022-10-23 22:36:35 +0300

    Move RemoteLoader composition to the Composition Root

--- EssentialFeed/EssentialFeedAPITests/Feed API/FeedItemsMapperTests.swift
+++ EssentialFeed/EssentialFeedAPITests/Feed API/FeedItemsMapperTests.swift
@@ -19,7 +19,7 @@ class FeedItemsMapperTests: XCTestCase {
         }
     }
     
-    func test_map_throwsErrorOn200HTTPResponseWithInvalidJSON() throws {
+    func test_map_throwsErrorOn200HTTPResponseWithInvalidJSON() {
         let invalidJSON = Data("invalid json".utf8)
         
         XCTAssertThrowsError(
