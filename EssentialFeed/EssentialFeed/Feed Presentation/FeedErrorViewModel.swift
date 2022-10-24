18a606d4d216835a2cb4c9141415976da91e358c
Mircea Dragota <mircea.dragota@x2mobile.net>
2022-10-24 12:19:11 +0300

    Duplicate FeedPresenter as LoadResourcePresenter

--- EssentialFeed/EssentialFeed/Feed Presentation/FeedErrorViewModel.swift
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedErrorViewModel.swift
@@ -10,12 +10,12 @@ import Foundation
 
 public struct FeedErrorViewModel {
     public let message: String?
-
-     static var noError: FeedErrorViewModel {
-         return FeedErrorViewModel(message: nil)
-     }
+    
+    static var noError: FeedErrorViewModel {
+        return FeedErrorViewModel(message: nil)
+    }
     
     static func error(message: String) -> FeedErrorViewModel {
         return FeedErrorViewModel(message: message)
     }
- }
+}
