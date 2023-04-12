936e0f9c6bc04ba56002b26d6ef433c24e0c3217
Mircea Dragota <mircea.dragota@x2mobile.net>
2023-04-12 10:50:07 +0300

    Load images synchronously

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedImageDataLoader.swift
@@ -9,66 +9,66 @@
 import Foundation
 
 public final class LocalFeedImageDataLoader {
-     private let store: FeedImageDataStore
-
-     public init(store: FeedImageDataStore) {
-         self.store = store
-     }
- }
+    private let store: FeedImageDataStore
+    
+    public init(store: FeedImageDataStore) {
+        self.store = store
+    }
+}
 
 extension LocalFeedImageDataLoader: FeedImageDataCache {
     public typealias SaveResult = FeedImageDataCache.Result
+    
+    public enum SaveError: Error {
+        case failed
+    }
+    
+    public func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
+        completion(SaveResult{
+            try store.insert(data, for: url)
+        }.mapError { _ in SaveError.failed })
+    }
+}
 
-     public enum SaveError: Error {
-         case failed
-     }
-
-     public func save(_ data: Data, for url: URL, completion: @escaping (SaveResult) -> Void) {
-         completion(SaveResult{
-             try store.insert(data, for: url)
-         }.mapError { _ in SaveError.failed })
-     }
- }
-
- extension LocalFeedImageDataLoader: FeedImageDataLoader {
-     public typealias LoadResult = FeedImageDataLoader.Result
-
-     public enum LoadError: Error {
-         case failed
-         case notFound
-     }
-
-     private final class LoadImageDataTask: FeedImageDataLoaderTask {
-         private var completion: ((FeedImageDataLoader.Result) -> Void)?
-
-         init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
-             self.completion = completion
-         }
-
-         func complete(with result: FeedImageDataLoader.Result) {
-             completion?(result)
-         }
-
-         func cancel() {
-             preventFurtherCompletions()
-         }
-
-         private func preventFurtherCompletions() {
-             completion = nil
-         }
-     }
-
-     public func loadImageData(from url: URL, completion: @escaping (LoadResult) -> Void) -> FeedImageDataLoaderTask {
-         let task = LoadImageDataTask(completion)
-         store.retrieve(dataForURL: url) { [weak self] result in
-             guard self != nil else { return }
-
-             task.complete(with: result
-                 .mapError { _ in LoadError.failed }
-                 .flatMap { data in
-                     data.map { .success($0) } ?? .failure(LoadError.notFound)
-                 })
-         }
-         return task
-     }
- }
+extension LocalFeedImageDataLoader: FeedImageDataLoader {
+    public typealias LoadResult = FeedImageDataLoader.Result
+    
+    public enum LoadError: Error {
+        case failed
+        case notFound
+    }
+    
+    private final class LoadImageDataTask: FeedImageDataLoaderTask {
+        private var completion: ((FeedImageDataLoader.Result) -> Void)?
+        
+        init(_ completion: @escaping (FeedImageDataLoader.Result) -> Void) {
+            self.completion = completion
+        }
+        
+        func complete(with result: FeedImageDataLoader.Result) {
+            completion?(result)
+        }
+        
+        func cancel() {
+            preventFurtherCompletions()
+        }
+        
+        private func preventFurtherCompletions() {
+            completion = nil
+        }
+    }
+    
+    public func loadImageData(from url: URL, completion: @escaping (LoadResult) -> Void) -> FeedImageDataLoaderTask {
+        let task = LoadImageDataTask(completion)
+        task.complete(
+            with: Swift.Result {
+                try store.retrieve(dataForURL: url)
+            }
+                .mapError { _ in LoadError.failed }
+                .flatMap { data in
+                    data.map { .success($0) } ?? .failure(LoadError.notFound)
+                })
+        
+        return task
+    }
+}
