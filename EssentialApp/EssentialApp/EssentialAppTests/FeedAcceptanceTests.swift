db87284040412c9b7c8cd5d3d2eff4fa05e2dbaa
Mircea Dragota <mircea.dragota@x2mobile.net>
2023-04-12 16:15:28 +0300

    Subscribe upstream store subscriptions in a background queue to avoid blocking the main queue (tests still run synchronously in the main queue with the immediate scheduler)

--- EssentialApp/EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
+++ EssentialApp/EssentialApp/EssentialAppTests/FeedAcceptanceTests.swift
@@ -12,7 +12,8 @@ import EssentialFeediOS
 @testable import EssentialApp
 
 class FeedAcceptanceTests: XCTestCase {
-    func test_onLaunch_displayRemoteFeedCustomerHasConnectivity() {
+    
+    func test_onLaunch_displaysRemoteFeedWhenCustomerHasConnectivity() {
         let feed = launch(httpClient: .online(response), store: .empty)
         
         XCTAssertEqual(feed.numberOfRenderedFeedImageViews(), 2)
@@ -37,8 +38,9 @@ class FeedAcceptanceTests: XCTestCase {
         XCTAssertFalse(feed.canLoadMoreFeed)
     }
     
-    func test_onLaunch_displaysCacheRemoteFeedWhenCustomerHasNoConnectivity() {
+    func test_onLaunch_displaysCachedRemoteFeedWhenCustomerHasNoConnectivity() {
         let sharedStore = InMemoryFeedStore.empty
+        
         let onlineFeed = launch(httpClient: .online(response), store: sharedStore)
         onlineFeed.simulateFeedImageViewVisible(at: 0)
         onlineFeed.simulateFeedImageViewVisible(at: 1)
@@ -75,7 +77,7 @@ class FeedAcceptanceTests: XCTestCase {
         XCTAssertNotNil(store.feedCache, "Expected to keep non-expired cache")
     }
     
-    func test_onFeedImageSelection_displayComments() {
+    func test_onFeedImageSelection_displaysComments() {
         let comments = showCommentsForFirstImage()
         
         XCTAssertEqual(comments.numberOfRenderedComments(), 1)
@@ -85,7 +87,7 @@ class FeedAcceptanceTests: XCTestCase {
     // MARK: - Helpers
     
     private func launch(httpClient: HTTPClientStub = .offline, store: InMemoryFeedStore = .empty) -> ListViewController {
-        let sut = SceneDelegate(httpClient: httpClient, store: store)
+        let sut = SceneDelegate(httpClient: httpClient, store: store, scheduler: .immediateOnMainQueue)
         sut.window = UIWindow(frame: CGRect(x: 0, y: 0, width: 1, height: 1))
         sut.configureWindow()
         
@@ -94,7 +96,7 @@ class FeedAcceptanceTests: XCTestCase {
     }
     
     private func enterBackground(with store: InMemoryFeedStore) {
-        let sut = SceneDelegate(httpClient: HTTPClientStub.offline, store: store)
+        let sut = SceneDelegate(httpClient: HTTPClientStub.offline, store: store, scheduler: .immediateOnMainQueue)
         sut.sceneWillResignActive(UIApplication.shared.connectedScenes.first!)
     }
     
@@ -106,6 +108,72 @@ class FeedAcceptanceTests: XCTestCase {
         
         let nav = feed.navigationController
         return nav?.topViewController as! ListViewController
+    }
+    
+    private func response(for url: URL) -> (Data, HTTPURLResponse) {
+        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
+        return (makeData(for: url), response)
+    }
+    
+    private func makeData(for url: URL) -> Data {
+        switch url.path {
+        case "/image-0": return makeImageData0()
+        case "/image-1": return makeImageData1()
+        case "/image-2": return makeImageData2()
+            
+        case "/essential-feed/v1/feed" where url.query?.contains("after_id") == false:
+            return makeFirstFeedPageData()
+            
+        case "/essential-feed/v1/feed" where url.query?.contains("after_id=A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A") == true:
+            return makeSecondFeedPageData()
+            
+        case "/essential-feed/v1/feed" where url.query?.contains("after_id=166FCDD7-C9F4-420A-B2D6-CE2EAFA3D82F") == true:
+            return makeLastEmptyFeedPageData()
+            
+        case "/essential-feed/v1/image/2AB2AE66-A4B7-4A16-B374-51BBAC8DB086/comments":
+            return makeCommentsData()
+            
+        default:
+            return Data()
+        }
+    }
+    
+    private func makeImageData0() -> Data { UIImage.make(withColor: .red).pngData()! }
+    private func makeImageData1() -> Data { UIImage.make(withColor: .green).pngData()! }
+    private func makeImageData2() -> Data { UIImage.make(withColor: .blue).pngData()! }
+    
+    private func makeFirstFeedPageData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: ["items": [
+            ["id": "2AB2AE66-A4B7-4A16-B374-51BBAC8DB086", "image": "http://feed.com/image-0"],
+            ["id": "A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A", "image": "http://feed.com/image-1"]
+        ]])
+    }
+    
+    private func makeSecondFeedPageData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: ["items": [
+            ["id": "166FCDD7-C9F4-420A-B2D6-CE2EAFA3D82F", "image": "http://feed.com/image-2"],
+        ]])
+    }
+    
+    private func makeLastEmptyFeedPageData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: ["items": []])
+    }
+    
+    private func makeCommentsData() -> Data {
+        return try! JSONSerialization.data(withJSONObject: ["items": [
+            [
+                "id": UUID().uuidString,
+                "message": makeCommentMessage(),
+                "created_at": "2020-05-20T11:24:59+0000",
+                "author": [
+                    "username": "a username"
+                ]
+            ],
+        ]])
+    }
+    
+    private func makeCommentMessage() -> String {
+        "a message"
     }
     
     private class HTTPClientStub: HTTPClient {
@@ -155,13 +223,12 @@ class FeedAcceptanceTests: XCTestCase {
             completion(.success(feedCache))
         }
         
-        func insert(_ data: Data, for url: URL, completion: @escaping (InsertionResult) -> Void) {
+        func insert(_ data: Data, for url: URL) throws {
             feedImageDataCache[url] = data
-            completion(.success(()))
         }
         
-        func retrieve(dataForURL url: URL, completion: @escaping (RetrievalResult) -> Void) {
-            completion(.success(feedImageDataCache[url]))
+        func retrieve(dataForURL url: URL) throws -> Data? {
+            feedImageDataCache[url]
         }
         
         static var empty: InMemoryFeedStore {
@@ -175,82 +242,5 @@ class FeedAcceptanceTests: XCTestCase {
         static var withNonExpiredFeedCache: InMemoryFeedStore {
             return InMemoryFeedStore(feedCache: CachedFeed(feed: [], timestamp: Date()))
         }
-    }
-    
-    private func response(for url: URL) -> (Data, HTTPURLResponse) {
-        let response = HTTPURLResponse(url: url, statusCode: 200, httpVersion: nil, headerFields: nil)!
-        return (makeData(for: url), response)
-    }
-    
-    private func makeData(for url: URL) -> Data {
-        switch url.path {
-        case "/image-0":
-            return makeImageData0()
-        case "/image-1":
-            return makeImageData1()
-        case "/image-2":
-            return makeImageData2()
-            
-        case "/essential-feed/v1/feed" where url.query?.contains("after_id") == false:
-            return makeFirstFeedPageData()
-            
-        case "/essential-feed/v1/feed" where url.query?.contains("after_id=A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A") == true:
-            return makeSecondFeedPageData()
-            
-        case "/essential-feed/v1/feed" where url.query?.contains("after_id=A28F5FE3-27A7-44E9-8DF5-53742D0E4A5B") == true:
-            return makeLastEmptyFeedPageData()
-            
-        case "/essential-feed/v1/image/2AB2AE66-A4B7-4A16-B374-51BBAC8DB086/comments":
-            return makeCommentsData()
-            
-        default:
-            return Data()
-        }
-    }
-    
-    private func makeImageData0() -> Data {
-        return UIImage.make(withColor: .red).pngData()!
-    }
-    
-    private func makeImageData1() -> Data {
-        return UIImage.make(withColor: .green).pngData()!
-    }
-    
-    private func makeImageData2() -> Data {
-        return UIImage.make(withColor: .blue).pngData()!
-    }
-    
-    private func makeFirstFeedPageData() -> Data {
-        return try! JSONSerialization.data(withJSONObject: ["items": [
-            ["id": "2AB2AE66-A4B7-4A16-B374-51BBAC8DB086", "image": "http://feed.com/image-0"],
-            ["id": "A28F5FE3-27A7-44E9-8DF5-53742D0E4A5A", "image": "http://feed.com/image-1"]
-        ]])
-    }
-    
-    private func makeSecondFeedPageData() -> Data {
-        return try! JSONSerialization.data(withJSONObject: ["items": [
-            ["id": "A28F5FE3-27A7-44E9-8DF5-53742D0E4A5B", "image": "http://feed.com/image-2"]
-        ]])
-    }
-    
-    private func makeLastEmptyFeedPageData() -> Data {
-        return try! JSONSerialization.data(withJSONObject: ["items": []])
-    }
-    
-    private func makeCommentsData() -> Data {
-        return try! JSONSerialization.data(withJSONObject: ["items": [
-            [
-                "id": UUID().uuidString,
-                "message": makeCommentMessage(),
-                "created_at": "2020-05-20T11:24:59+0000",
-                "author": [
-                    "username": "a username"
-                ]
-            ],
-        ]])
-    }
-    
-    private func makeCommentMessage() -> String {
-        "a message"
     }
 }
